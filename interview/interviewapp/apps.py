0c1d9dc0b8e996f53ed61bcb4245d8d791ee9596
girinetha <144089509+girinetha@users.noreply.github.com>
2024-01-28 12:40:02 +0530

    Delete interview directory

--- interview/interviewapp/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class InterviewappConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'interviewapp'
--- interview/interviewapp/views.py
+++
@@ -1,5 +0,0 @@
-from django.shortcuts import render
-from django.http import HttpRequest,HttpResponse
-# Create your views here.
-def display(request):
-    return HttpResponse("<h1>successfully loaded</h1>")
